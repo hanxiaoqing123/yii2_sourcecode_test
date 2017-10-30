2347446f1ef174558613977d390c819103b05a4f
hanxiaoqing123 <1695722217@qq.com>
2017-10-30 16:13:56 +0800

    Ioc容器 add 备注

--- Ioc/reflectioncontainer.php
+++ Ioc/reflectioncontainer.php
@@ -8,20 +8,50 @@ class Container{
 
     public function build($class,$params)
     {
-        $dependencies=[];
+        $dependencies=[];   //其他依赖
         $reflection=new ReflectionClass($class);
-        $constuctor=$reflection->getConstructor();
+        $constuctor=$reflection->getConstructor();  //获取类的构造函数
+        /*
+         var_dump($constuctor);
+         object(ReflectionMethod)[3]
+             public 'name' => string '__construct' (length=11)
+             public 'class' => string 'User' (length=4)
+         * */
         if($constuctor!=null){
+            //继承自 ReflectionFunctionAbstract::getParameters — 获取参数
+            /*
+             var_dump($constuctor->getParameters());
+             array (size=1)
+                0 =>
+                    object(ReflectionParameter)[4]
+                    public 'name' => string 'emailSenderObject' (length=17)
+             * */
             foreach ($constuctor->getParameters() as $param){
+               //获取反射类
                 $c=$param->getClass();
+                /*
+                 var_dump($c);
+                 object(ReflectionClass)[5]
+                     public 'name' => string 'EmailSenderBy163' (length=16)
+                 * */
                 if($c!=null){
                     $dependencies[]=$this->get($c->getName(),$params);
+                    /*获取类名
+                      var_dump($c->getName());
+                      string 'EmailSenderBy163' (length=16)
+                     * */
                 }
             }
         }
         foreach ($params as $index=>$param){
             $dependencies[$index]=$param;
         }
+        /*
+         * var_dump($dependencies);die;
+         array (size=1)
+        'EmailSenderBy163' => string 'this is name' (length=12)
+         * */
+
         //从给出的参数创建一个新的类实例
         return $reflection->newInstanceArgs($dependencies);
     }
