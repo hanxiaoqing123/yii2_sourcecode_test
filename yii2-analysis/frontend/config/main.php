d7a41c8c490893fc8b48fab906ac8343724a6156
hanxiaoqing123 <1695722217@qq.com>
2020-03-21 19:58:04 +0800

    源码-行为分析

--- yii2-analysis/frontend/config/main.php
+++ yii2-analysis/frontend/config/main.php
@@ -12,7 +12,7 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
-    'as A'=>\frontend\components\A::className(),
+    'as A'=>frontend\components\A::className(),
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-frontend',
